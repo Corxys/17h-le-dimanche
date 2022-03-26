bd3d3bfb91a285c4fdb01a8f3aa8e62b9c86b3d5
iroshiva <jean.weil@protonmail.com>
2022-03-26 15:43:57 +0100

    Set form + home page + test route

--- nuxt.config.js
+++ nuxt.config.js
@@ -38,7 +38,10 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module'
+    // '@nuxtjs/eslint-module'
+
+    // Simple usage
+    '@nuxtjs/router-extras'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
---
+++ src/pages/home/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Accueil</h1>
+    <NuxtLink to="/formulaire">
+      Formulaire
+    </NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
+
+<router>
+  {
+    path: '/accueil'
+  }
+</router>
