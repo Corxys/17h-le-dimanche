135f95d13887644feeccf3d8db9bb274bbcd5215
iroshiva <jean.weil@protonmail.com>
2022-03-26 15:45:21 +0100

    Set button component

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+  <button class="button">
+    {{ value }}
+  </button>
+</template>
+<script>
+export default {
+  name: 'ButtonComponent',
+  props: ['value']
+}
+</script>
+<style scoped>
+  .button {
+    background-color: pink;
+    color: white;
+    padding: 15px 25px;
+    border-radius: 50px;
+  }
+</style>
