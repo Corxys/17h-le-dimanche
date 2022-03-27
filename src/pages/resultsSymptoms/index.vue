4c45351eabadff4aa82c7b7aacd1e10037cd5421
Corxis <jennblngr@gmail.com>
2022-03-27 03:48:39 +0200

    feat:survey results page

--- src/pages/resultsSymptoms/index.vue
+++ src/pages/resultsSymptoms/index.vue
@@ -1,28 +1,76 @@
 <template>
   <div class="results">
+    <h1 class="results__title">
+      Nous vous conseillons
+    </h1>
     <div class="results__cards">
       <div class="results__card">
-        CARTE 1 WALAH
+        <h2 class="results__card-title">Sophrologie</h2>
+        <p class="results__card-description">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+        <ButtonSimple value="Plus" />
       </div>
       <div class="results__card">
-        CARTE 2 AYA
+        <h2 class="results__card-title">Naturopathie</h2>
+        <p class="results__card-description">Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
+        <ButtonSimple value="Plus" />
       </div>
     </div>
     <p class="results__text">
       C'est bon, je suis renseignée !
     </p>
-    <ButtonSimple value="Continuer" />
+    <router-link to="/questionnaire-preferences">
+      <ButtonSimple value="Continuer" />
+    </router-link>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'ResultsSympatomsPage'
+  name: 'ResultsSymptomsPage'
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .results {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 50px 30px;
 
+    &__cards {
+      margin-bottom: 80px;
+    }
+
+    &__card {
+      display: flex;
+      flex-direction: column;
+      background-color: #f3ebd6;
+      border-radius: 15px;
+      padding: 30px;
+      margin-bottom: 15px;
+
+      &:last-child {
+        margin-bottom: 0px;
+      }
+
+      &-title {
+        margin-bottom: 15px;
+      }
+
+      &-description {
+        margin-bottom: 20px;
+      }
+
+      button {
+        align-self: flex-end;
+      }
+    }
+
+    &__text {
+      text-align: center;
+      margin-bottom: 10px;
+    }
+  }
 </style>
 
 <router>
--- src/store/survey/actions.js
+++ src/store/survey/actions.js
@@ -1,5 +1,9 @@
 export default {
   sendSurveySymptomsRequest (context) {
-    console.log('context')
+    const dict_of_values = {}
+    const stringifyData = JSON.stringify(dict_of_values)
+    console.log(stringifyData)
+
+    // axios.post('', dict_of_values, { headers: { 'Content-Type': 'application/json' }})
   }
 }
